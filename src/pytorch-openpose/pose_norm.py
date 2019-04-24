5914130b7470c8e7c0750041b3197da792a8b660
Neev Parikh <neev_parikh@brown.edu>
2019-04-24 17:41:21 -0400

    Added the alpha in formula

--- src/pytorch-openpose/pose_norm.py
+++ src/pytorch-openpose/pose_norm.py
@@ -3,21 +3,37 @@ import numpy as np
 class PoseNormalizer:
     ''' Normalizes the pose as described in the Everybody Dance Now paper '''
 
-    def __init__(self, source, target, epsilon=0.7):
+    def __init__(self, source, target, epsilon=0.7, inclusion_threshold=10):
         """
             source :: dict<ndarray> :: dict of source left ankle array and source right ankle array
             target :: dict<ndarray> :: dict of target left ankle array and target right ankle array
             epsilon :: float [0, 1] :: value for the clustering in calculating the min, paper suggests 0.7  
         """
 
-        self.s_left = source["left"]
-        self.s_right = source["right"]
-        self.t_left = target["left"]
-        self.t_right = target["right"]
+        self.inclusion_threshold = inclusion_threshold
+        self.s_left, self.s_right = self._include_ground_only(source["left"], source["right"]) 
+        self.t_left, self.t_right = self._include_ground_only(target["left"], target["right"]) 
         self.epsilon = epsilon
 
         self._compute_statistics(np.append(self.s_left, self.s_right), "source")
         self._compute_statistics(np.append(self.t_left, self.t_right), "target")
+
+    
+    def _include_ground_only(self, left_ankle_array, right_ankle_array):
+        """ remove the frames where the leg is raised """
+
+        num_frames = len(left_ankle_array)
+        left_grounded = [] 
+        right_grounded = []
+
+        for i in range(num_frames):
+            if np.abs(left_ankle_array[i] - right_ankle_array[i]) < self.inclusion_threshold:
+                left_grounded.append(left_ankle_array[i])
+                right_grounded.append(right_ankle_array[i])
+            else:
+                pass
+
+        return np.array(left_grounded), np.array(right_grounded)
 
     def _compute_translation(self, source):
         """ b = t_min + (avg_frame_pos_source - s_min) / (s_max - s_min) * (t_max - t_min) - f_source """
@@ -33,6 +49,7 @@ class PoseNormalizer:
 
     def _compute_scale(self, source):
         """ s = t_far / s_far + (a_source - s_min) / (s_max - s_min) * (t_close / s_close - t_far / s_far) """
+
         avg_source = (source["left"] + source["right"]) / 2
         t_far = self.statistics["target"]["far"]
         t_close = self.statistics["target"]["close"]
@@ -45,33 +62,33 @@ class PoseNormalizer:
 
         return (t_far / s_far) + (avg_source - s_min) / (s_max - s_min) * ((t_close / s_close) - (t_far / s_far))
 
-    def _compute_statistics(self, ankle_series, ankle_name):
+    def _compute_statistics(self, ankle_array, ankle_name):
         self.statistics = {}
-        med = self._get_median_ankle_position(ankle_series)
-        mx = self._get_max_ankle_position(ankle_series)
+        med = self._get_median_ankle_position(ankle_array)
+        mx = self._get_max_ankle_position(ankle_array)
         self.statistics[ankle_name] = {
             "med": med,
             "max": mx
         }
-        mn = self._get_min_ankle_position(ankle_series, med, mx)
+        mn = self._get_min_ankle_position(ankle_array, med, mx)
         self.statistics[ankle_name]["min"] = mn
-        self.statistics[ankle_name]["close"], self.statistics[ankle_name]["far"] = self._get_close_far_position(ankle_series, mx, mn)
+        self.statistics[ankle_name]["close"], self.statistics[ankle_name]["far"] = self._get_close_far_position(ankle_array, mx, mn)
     
-    def _get_median_ankle_position(self, ankle_series):
-        return np.median(ankle_series, overwrite_input=False)
+    def _get_median_ankle_position(self, ankle_array):
+        return np.median(ankle_array, overwrite_input=False)
     
-    def _get_min_ankle_position(self, ankle_series, med, mx):
+    def _get_min_ankle_position(self, ankle_array, med, mx):
         dist_mx_med = np.abs(mx - med)/mx
-        cluster = np.array([p for p in ankle_series if (p < med) and (np.abs(np.abs(p - med) - dist_mx_med) < self.epsilon)])
+        cluster = np.array([p for p in ankle_array if (p < med) and (np.abs(np.abs(p - med) - dist_mx_med) < self.epsilon)])
         return np.max(cluster)
 
-    def _get_close_far_position(self, ankle_series, mx, mn):
-        cluster_far = np.array([p for p in ankle_series if (np.abs(p - mn) < self.epsilon)])
-        cluster_close = np.array([p for p in ankle_series if (np.abs(p - mx) < self.epsilon)])
+    def _get_close_far_position(self, ankle_array, mx, mn):
+        cluster_far = np.array([p for p in ankle_array if (np.abs(p - mn) < self.epsilon)])
+        cluster_close = np.array([p for p in ankle_array if (np.abs(p - mx) < self.epsilon)])
         return np.max(cluster_close), np.max(cluster_far)
 
-    def _get_max_ankle_position(self, ankle_series):
-        return np.amax(ankle_series)
+    def _get_max_ankle_position(self, ankle_array):
+        return np.amax(ankle_array)
 
     def transform_pose(self, source, target):
         """
