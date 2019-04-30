06ddc1fca15dd3730f4737bea2766f137146df96
Neev Parikh <neev_parikh@brown.edu>
2019-04-30 15:24:59 -0400

    Pipeline working

--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
 import sys
-sys.path.insert(0, 'pytorch-openpose')
+import cv2
+sys.path.insert(0, "pytorch-openpose")
 import argparse
-from pose import get_normalized_target_pose
+import pickle
+from pose import get_pose_normed_estimatation, get_pose_estimatation 
 
+# Creates command line parser
 def main():
-	# Creates command line parser
-	parser = argparse.ArgumentParser()
-	parser.add_argument('source', help='path to source video')
-	parser.add_argument('target', help='path to target video')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", help="path to source video")
+    parser.add_argument("target", help="path to target video")
+    parser.add_argument("--mode", choices=["train", "transfer"], required=True, help="training mode or transfer mode")
+    args = parser.parse_args() 
 
-	args = parser.parse_args()    
-	source = args.source
-	target = args.target
+    source = args.source
+    target = args.target
+    mode = args.mode
 
-	try:
-		source_file = open(source)
-		target_file = open(target)
-	except FileNotFoundError:
-		print('Video files could not be found')
-		sys.exit()
-	
-	norm_poses = get_normalized_target_pose(source, target)
-    
+    if mode == "train":
+        target_poses = get_pose_estimatation(target)
+        # gan_model
+    else:
+        norm_source_poses = get_pose_normed_estimatation(source, target)[0]
+        try:
+            gan_model = pickle.load(open("gan/trained_gan.pkl", "rb"))
+        except FileNotFoundError:
+            print("GAN model not found. Please retrain the GAN.") 
+            sys.exit()
+        transfered_video = gan_model.apply_mapping(norm_source_poses)
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        video = cv2.VideoWriter('video/pose_normed.mp4', fourcc, 30,(720, 480))
+        for frame in transfered_video:
+            video.write(frame)
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
--- src/pytorch-openpose/pose.py
+++ src/pytorch-openpose/pose.py
@@ -1,4 +1,5 @@
 import sys
+import os
 sys.path.insert(0, 'pytorch-openpose/python')
 import cv2
 import model
@@ -10,17 +11,71 @@ import matplotlib.pyplot as plt
 import copy
 import numpy as np
 
-def get_normalized_target_pose(source, target):
+# TODO:
+# 1) Remove the rotation and move it to a separate preprocessing file
+
+def get_pose_estimate(video_location):
     """
-    source :: location of source video
-    target :: location of target video
-    returns python list of normalized poses for the target
+    video_location :: location of video
+    Returns python list of poses for the video
     """
+
+    if os.path.isfile(video_location): 
+        video = cv2.VideoCapture(video_location)
+    else: 
+        raise FileNotFoundError 
 
     body_estimation = Body('model/body_pose_model.pth')
 
-    source_video = cv2.VideoCapture(source)
-    target_video = cv2.VideoCapture(target)
+    # Initialize arrays to store pose information
+    poses = []
+
+    # Frame counter
+    frame = 0
+
+    while(True): 
+        # reading from frame 
+        ret_source, source_frame = source_video.read() 
+            
+        if ret_source and ret_target:
+            source_frame = np.rot90(np.rot90(np.rot90(source_frame)))
+            h, w, _ = source_frame.shape
+            source_frame = cv2.resize(source_frame, (int(w/2), int(h/2)))
+
+            target_frame = np.rot90(np.rot90(np.rot90(target_frame)))
+            target_frame = cv2.resize(target_frame, (int(w/2), int(h/2)))
+
+            # Grab pose estimations for both video frames
+            source_candidate, _ = body_estimation(source_frame)
+            target_candidate, _ = body_estimation(target_frame)
+
+            # Put pose estimations into memory
+            source_poses.append(source_candidate)
+            target_poses.append(target_candidate)
+
+            frame += 1
+            print("Frame: ", frame)
+        else:
+            break
+
+    source_video.release()  
+    target_video.release()
+
+    return source_poses, target_poses
+
+def get_pose_normed_estimatation(source, target):
+    """
+    source :: location of source video
+    target :: location of target video
+    Returns python list of normalized poses for the source
+    """
+    if os.path.isfile(source) and os.path.isfile(target):
+        source_video = cv2.VideoCapture(source)
+        target_video = cv2.VideoCapture(target)
+    else: 
+        raise FileNotFoundError 
+
+    body_estimation = Body('model/body_pose_model.pth')
 
     # Initialize arrays to store pose information
     source_poses = []
@@ -63,7 +118,7 @@ def get_normalized_target_pose(source, target):
             target_right.append(target_candidate[10, 1])
 
             frame += 1
-            print(frame)
+            print("Frame: ", frame)
         else:
             break
 
@@ -84,4 +139,4 @@ def get_normalized_target_pose(source, target):
     pose_normalizer = PoseNormalizer(source_dict, target_dict, epsilon=3)
     transformed_all = pose_normalizer.transform_pose_global(source_poses, target_poses)
     
-    return transformed_all
+    return transformed_all, source_poses, target_poses
