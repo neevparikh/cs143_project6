65b6acb1a3cbb639ba78138c4ed89538d56c8316
rgreenblatt <greenblattryan@gmail.com>
2019-05-03 19:25:30 -0400

    Fixed missing imwrite

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@ from data.data_loader import CreateDataLoader
 from options.test_options import TestOptions
 from models.models import create_model
 import os
+from cv2 import imwrite
 
 def generate(config, writer, logger):
     config = config.opt
@@ -12,7 +13,7 @@ def generate(config, writer, logger):
     for i, data in enumerate(data_set):
         minibatch = 1
         generated = model.inference(data['label'], data['inst'])
-        os.path.join("outputs", "output_{}.png".format(i), generated)
+        imwrite(os.path.join("outputs", "output_{}.png".format(i)), generated)
 
 if __name__ == '__main__':
     generate(*utils.start_run(TestOptions))
