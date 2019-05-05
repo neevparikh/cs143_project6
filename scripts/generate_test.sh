9acecc5cbfc4c2435fd21b778d9c7146b763721e
rgreenblatt <greenblattryan@gmail.com>
2019-05-05 23:41:38 +0000

    Fixed args

--- scripts/generate_test.sh
+++ scripts/generate_test.sh
@@ -1,3 +1,2 @@
  #!/bin/bash
- python3 src/generate_test_data.py --rotated=true --regen_target=true \
-   --regen_source=true --regen_norm=true $@ 
+ python3 src/generate_test_data.py --rotated=true $@ 
