eb7b22ee7c1c2230a73b7583ec385d219e895ba2
Neev Parikh <neev_parikh@brown.edu>
2019-05-06 11:10:19 -0400

    Fixed inference script

--- src/test.py
+++ src/test.py
@@ -11,6 +11,7 @@ import util.util as util
 from collections import OrderedDict
 from util.visualizer import Visualizer
 from util import html
+import torch
 
 def generate(config, writer, logger):
     config = config.opt
@@ -24,19 +25,44 @@ def generate(config, writer, logger):
     webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' %
                         (config.name, config.phase, config.which_epoch))
 
+    is_first = True
+
     for data in tqdm(data_set):
-        data['label'] = data['label'][:,:1]
-        assert data['label'].shape[1] == 1
+        if config.no_temporal_smoothing:
+            data['label'] = data['label'][:,:1]
+            assert data['label'].shape[1] == 1
 
-        generated = model.inference(data['label'], data['inst'])
-        visuals = OrderedDict([('input_label',
-                                util.tensor2label(data['label'][0],
-                                                   config.label_nc)),
-                               ('synthesized_image',
-                                util.tensor2im(generated.data[0]))])
+            generated = model.inference(data['label'], data['inst'])
+            visuals = OrderedDict([('input_label',
+                                    util.tensor2label(data['label'][0],
+                                                       config.label_nc)),
+                                   ('synthesized_image',
+                                    util.tensor2im(generated.data[0]))])
 
-        img_path = data['path']
-        visualizer.save_images(webpage, visuals, img_path)
+            img_path = data['path']
+            visualizer.save_images(webpage, visuals, img_path)
+        else:
+            data['label'] = data['label'][:,:1]
+            assert data['label'].shape[1] == 1
+
+            if is_first:
+                prev_generated = torch.zeros_like(data['label'])
+
+            generated = model.inference(torch.cat((data['label'], prev_generated),
+                dim=1), data['inst'])
+
+            prev_generated = generated
+            is_first = False
+
+            visuals = OrderedDict([('input_label',
+                                    util.tensor2label(data['label'][0],
+                                                       config.label_nc)),
+                                   ('synthesized_image',
+                                    util.tensor2im(generated.data[0]))])
+
+            img_path = data['path']
+            visualizer.save_images(webpage, visuals, img_path)
+
 
 if __name__ == '__main__':
     generate(*utils.start_run(TestOptions))
