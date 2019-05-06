2d8b430233800dc6114ceeb3c673ab5795560cf8
rgreenblatt <greenblattryan@gmail.com>
2019-05-06 13:49:54 -0400

    Fixed sequencing

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 src/test.py --dataroot "data/" --label_nc=18  --no_instance --verbose --nThreads=0 --loadSize=512 $@
+python3 src/test.py --dataroot "data/" --label_nc=18  --no_instance --verbose --nThreads=0 --loadSize=512 --serial_batches $@
