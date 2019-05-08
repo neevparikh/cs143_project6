7f7b47ad62c58e15f2f82ef2c88a721c0e7c2ae8
rgreenblatt <greenblattryan@gmail.com>
2019-05-08 17:09:02 +0000

    Fixed num gpus

--- scripts/train_distributed.sh
+++ scripts/train_distributed.sh
@@ -2,6 +2,6 @@
 
 #change 2 to num gpus
 
-python3 -m  torch.distributed.launch --nproc_per_node=2 src/train.py \
+python3 -m  torch.distributed.launch --nproc_per_node=4 src/train.py \
   --dataroot="data/" --label_nc=18  --no_instance --verbose --loadSize=512 \
   --no_flip --serial_batches --fp16 $@
